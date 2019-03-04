650aaf4d6ee8d5da1aac877ef08f07fd6004453b
Coty Condry <ccondry@cisco.com>
2019-03-04 13:47:22 -0600

    fix update script file destination

--- update.sh
+++ update.sh
@@ -23,7 +23,7 @@ else
         #echo "removing old web files"
         #rm -rf /var/www/html/
         echo "copying new web files"
-        cp -rf dist/* /var/www/toolbox/pcce/
+        cp -rf dist/* /var/www/toolbox/brand/
       else
         echo "npm failed to run build script"
       fi
