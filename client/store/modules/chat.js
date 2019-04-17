9779baeebcbfbbfed898be15def2482d00338d94
Coty Condry <ccondry@cisco.com>
2019-04-17 11:34:04 -0500

    pop our own static HTML file in a new window to start CJP CCOne chat

--- client/store/modules/chat.js
+++ client/store/modules/chat.js
@@ -1,4 +1,5 @@
 // import { Toast } from 'buefy/dist/components/toast'
+import queryString from 'query-string'
 
 const getters = {
 }
@@ -11,49 +12,39 @@ const mutations = {
 
 const actions = {
   popCconeChatWindow ({getters}, data) {
-    // cc one chat parameters
-    const PROTOCOL = 'https'
-    const IP = 'mm.ccone.net'
-    const PORT = 443
-    const EXCEPTION_MESSAGE = 'Unable to process your request.'
-    const SHOW_EXCEPTION_DETAILS = false
-
-    // init the CCOne chat system
-    const chatAPI = new window.ThinConnectChat(PROTOCOL, IP, PORT, EXCEPTION_MESSAGE, SHOW_EXCEPTION_DETAILS)
-
-    // attach data
-    const dataArr = []
-    dataArr.push({ key: 'custname', value: data.name })
-    dataArr.push({ key: 'mobilenum', value: data.phone })
-    dataArr.push({ key: 'emailid', value: data.email })
-    dataArr.push({ key: 'question', value: data.message })
-    dataArr.push({ key: 'reason', value: getters.sessionConfig.reasonId })
-    dataArr.push({ key: 'tenantId', value: getters.sessionConfig.tenantId })
-    dataArr.push({ key: 'sendtranscript', value: 'false' })
-    dataArr.push({ key: 'CUSTOM_MESSAGE', value: 'Unable to process your request.' })
-    dataArr.push({ key: 'SHOW_EXCEPTION_DETAILS', value: false })
-    dataArr.push({ key: 'REQUEST-ID', value: 0 })
-    dataArr.push({ key: 'sourceUrl', value: '' })
-    dataArr.push({ key: 'chatRsnSelMode', value: '' })
-    dataArr.push({ key: 'websitePkey', value: '' })
-    dataArr.push({ key: 'defaultChatReason', value: '' })
-    dataArr.push({ key: 'traversalHistory', value: '' })
-    dataArr.push({ key: 'customerId', value: '' })
-    dataArr.push({ key: 'groupId', value: '' })
-
-    dataArr.push({ key: 'data0', value: 'Cumulus Homepage' })
-    dataArr.push({ key: 'data1', value: 'Model_X Diamond Customer' })
-    dataArr.push({ key: 'data2', value: 'Finance Quote' })
-    dataArr.push({ key: 'data3', value: '' })
-    dataArr.push({ key: 'data4', value: '' })
-    dataArr.push({ key: 'data5', value: '' })
-    dataArr.push({ key: 'data6', value: '' })
-    dataArr.push({ key: 'data7', value: '' })
-    dataArr.push({ key: 'data8', value: '' })
-    dataArr.push({ key: 'data9', value: '' })
-
+    console.log('popCconeChatWindow', data)
+    // validate
+    if (isNaN(getters.sessionConfig.reasonId) || isNaN(getters.sessionConfig.tenantId)) {
+      // reasonId or tenantId are not set - pop error message
+      console.log('popCconeChatWindow - reasonId or tenantId are not set. Not starting chat.')
+      // stop here - don't pop chat window without all the params
+      return
+    }
+    // build URL query parameter options
+    const query = {
+      name: data.name,
+      phone: data.phone,
+      email: data.email,
+      question: data.message,
+      reasonId: getters.sessionConfig.reasonId,
+      tenantId: getters.sessionConfig.tenantId,
+      data0: 'Cumulus Homepage',
+      data1: 'Model_X Diamond Customer',
+      data2: 'Finance Quote'
+    }
+    console.log('popCconeChatWindow - starting chat with query =', query)
+    // make a URL-friendly query string from JSON data
+    const qs = queryString.stringify(query)
+    // build the URL
+    const url = `https://mm-static.cxdemo.net/ccone-chat?${qs}`
+    console.log('popCconeChatWindow - starting chat with full url =', url)
     // pop the chat window
-    chatAPI.submit(dataArr)
+    let w = 400
+    let h = 600
+    let top = (window.screen.height / 2) - (h / 2)
+    let left = (window.screen.width / 2) - (w / 2)
+    window.open(url, '_blank', `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${top}, left=${left}`)
+    console.log('popCconeChatWindow - chat window opened.')
   },
   popUpstreamChatWindow ({getters}, data) {
     console.log('popUpstreamChatWindow', data)
