82372280cfb40fe12c1deafecce9970ca655092e
Coty Condry <ccondry@cisco.com>
2019-12-06 12:28:35 -0600

    add npm rebuild node-sass to script before doing npm run build

--- update.sh
+++ update.sh
@@ -16,16 +16,22 @@ else
     echo "running npm install"
     npm i
     if [ $? -eq 0 ]; then
-      echo "running npm run build..."
-      npm run build
+      echo "running npm rebuild node-sass..."
+      npm rebuild node-sass
       if [ $? -eq 0 ]; then
-        echo "npm build successful"
-        #echo "removing old web files"
-        #rm -rf /var/www/html/
-        echo "copying new web files"
-        cp -rf dist/* /var/www/html/brand/
+        echo "running npm run build..."
+        npm run build
+        if [ $? -eq 0 ]; then
+          echo "npm build successful"
+          #echo "removing old web files"
+          #rm -rf /var/www/html/
+          echo "copying new web files"
+          cp -rf dist/* /var/www/html/brand/
+        else
+          echo "npm failed to run build script"
+        fi
       else
-        echo "npm failed to run build script"
+        echo "npm rebuild node-sass failed"
       fi
     else
       echo "failed npm install"
